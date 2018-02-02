a108663953bcc254e17966a9139bfbf9b06a432c
Brian Lundell <atltt73@gmail.com>
2018-02-01 19:48:23 -0500

    Trans2 walks tthrough all CSV Files and

---
+++ Trans2.py
@@ -0,0 +1,42 @@
+from googletrans import Translator
+import csv
+import os
+
+translater = Translator()
+
+#Sets path to files and outbound file path
+path = '/home/atltt/Documents/SideProjects/TwitterData/holding'
+path2 = '/home/atltt/Documents/SideProjects/TwitterData/holding/OUT'
+
+for fname in os.listdir('/home/atltt/Documents/SideProjects/TwitterData/holding'):
+    if fname.endswith(".csv"):
+        f = open(path + "/" + fname, "rb")
+        csv_f = csv.reader(f)
+        g = open(path2 + "/" + "OUT" + fname, "w")
+        csv_wf = csv.writer(g)
+        tmp = {}
+        x = 0
+        heading = csv_f.next()
+        heading.append('Translation')
+        csv_wf.writerow(heading)
+
+        for row in csv_f:
+            try:
+                tmp[x] = translater.translate(row[4], dest="en")
+                row.append(tmp[x].text.encode('utf-8'))
+                csv_wf.writerow(row)
+                #Used these to guage if still working.
+                #x = x + 1
+                #print(x)
+            except Exception():
+                pass
+            except ValueError:
+                #weird error, text seems normal but throws error
+                #Tried working this to print out the error items
+                #but it wouldn't print, will look into further
+                #print(tmp[x].text)
+                pass
+
+
+        f.close()
+        g.close()
