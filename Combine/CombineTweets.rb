29caf61676c8a1b6e50c8056e886942b20cc4b30
Audris Mockus <audris@utk.edu>
2018-07-13 14:48:06 -0400

    Fixed cleaning code

--- Combine/CombineTweets.rb
+++ Combine/CombineTweets.rb
@@ -10,17 +10,20 @@ f.write("'username'|'date'|'retweets'|'favorites'|'text'|'cleantext'|'is_retweet
 for filename in Dir.glob(File.join(INPUT_DIR, "*")) do
 	data = JSON.parse(IO.read(filename))
 		text = data["text"]
-		textClean = text
-		textClean = textClean.gsub(/pic.twitter.com\/[^ ]*/,"")
+		
 		text = text.gsub("http:// ","http://")
 		text = text.gsub("https:// ","https://")
 		text = text.gsub("http://www ","http://www")
-		text = text.gsub("https://www ","https://www") 
-		textClean = textClean.gsub(/https:\/\/[^ ]*/,"")
-		textClean = textClean.gsub(/http:\/\/[^ ]*/,"")
+		text = text.gsub("https://www ","https://www")
 		text = text.gsub("\r","__CR__")
 		text = text.gsub("\n","__NEWLINE__")
 		text = text.gsub("|","__PIPE__")
+	
+	        textClean = text
+		textClean = textClean.gsub(/pic.twitter.com\/[^ ]*/,"")
+		textClean = textClean.gsub(/https:\/\/[^ ]*/,"")
+		textClean = textClean.gsub(/http:\/\/[^ ]*/,"")
+
 		data["text"] = text; 
 		f.write("\n%s|%s|%d|%d|%s|%s|%s|%s|%s" % [data["usernameTweet"], data["datetime"], data["nbr_retweet"], data["nbr_favorite"], data["text"], textClean, data["is_retweet"], data["ID"], data["url"]])
 end
