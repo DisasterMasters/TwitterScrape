906d2f5caabeb9fc0bb74c5f8fa8c8e450c36de0
Sai Thatigotla <sai.thatigotla@gmail.com>
2018-12-20 12:35:00 -0600

    updated label in adweek_scraper.py

--- Scraper/TweetScraper/Spiders/adweek_scraper.py
+++ Scraper/TweetScraper/Spiders/adweek_scraper.py
@@ -2,14 +2,12 @@ import scrapy
 
 class AdweekScraper(scrapy.Spider):
     name="AdweekScraper"
-    def start_requests(self):
-        allowed_domains = ["adweek.com"]
-        start_urls = [
-            'https://www.adweek.com/digital/twitter-breaking-news/'
-            ]
+    start_urls = [
+        'https://www.adweek.com/digital/twitter-breaking-news/'
+        ]
 
     def parse(self, response):
         for link in response.xpath("//div[@class='post-text']/p/strong/a"):
             yield{
-                'header': link.xpath("text()").extract()[0]
+                'handles': link.xpath("text()").extract()[0]
             }
